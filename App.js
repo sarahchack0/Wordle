6b2072dd2d8fc9dbcc62c9c61f106bf52f62dc85
Sarah C <sarah.m.chacko.23@dartmouth.edu>
2023-05-06 15:09:18 -0400

    make one row of wordle

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import {colors} from './src/constants'
+import KeyBoard from './src/components/Keyboard'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" />
+      <Text style={styles.title}>WORDLE</Text>
+      <View style={styles.map}>
+        <View style={styles.row}>
+          <View style={styles.cell}/>
+          <View style={styles.cell}/>
+          <View style={styles.cell}/>
+          <View style={styles.cell}/>
+          <View style={styles.cell}/>
+        </View>
+      </View>
+      <KeyBoard />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.black,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    color: colors.lightgrey,
+    fontSize:32,
+    fontWeight: "bold",
+    letterSpacing: 7,
+  },
+  map: {
+    alignSelf: 'stretch',
+    height: 100,
+  },
+  row: {
+    alignSelf: "stretch",
+    flexDirection: 'row',
+  }, 
+  cell: {
+    borderWidth:3,
+    borderColor:colors.darkgrey,
+    flex: 1, // divide space with siblings
+    aspectRatio:1, // make the cell square
+    margin: 3,
   },
 });
