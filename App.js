846f16b3260f62b595bfcf348912a1cc7fbed8bf
Sarah C <sarah.m.chacko.23@dartmouth.edu>
2023-05-06 15:56:14 -0400

    adding letter colors

--- App.js
+++ App.js
@@ -1,17 +1,73 @@
+import { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
-import {colors} from './src/constants'
+import {colors, CLEAR, ENTER} from './src/constants'
 import KeyBoard from './src/components/Keyboard'
 
 const NUMBER_OF_TRIES = 6;
+
+const copyArray = (arr) => {
+  return [...arr.map((rows) => [...rows])];
+}
+
 
 export default function App() {
   const word = "hello";
   const letters = word.split(''); // return array of characters 
 
-  const rows = new Array(NUMBER_OF_TRIES).fill(new Array(letters.length).fill('a'));
+  const [rows, setRows] = useState(new Array(NUMBER_OF_TRIES).fill(new Array(letters.length).fill('')));
+  const [curRow, setCurRow] = useState(0);
+  const [curCol, setCurCol] = useState(0);
+
+  const isCellActive = (row, col) => {
+    return row === curRow && col === curCol;
+  }
   // rows is array of row
   // row contains empty cells
+
+  const getCellBGColor = (letter, row, col) => {
+    if (row >= curRow) {
+      return colors.black;
+    }
+    if (letter === letters[col]) {
+      return colors.primary;
+    }
+    if (letters.includes(letter)) {
+      return colors.secondary;
+    }
+    return colors.darkgrey;
+  }
+
+  const onKeyPressed = (key) => {
+    const updatedRows = copyArray(rows);
+
+    if (key === CLEAR) {
+      const prevCol = curCol - 1;
+      if (prevCol >= 0) {
+        updatedRows[curRow][prevCol] = '';
+        setRows(updatedRows);
+        setCurCol(prevCol);
+      }
+      return;
+    }
+
+    if (key === ENTER) {
+      if (curCol === rows[0].length) {
+        setCurRow(curRow + 1);
+        setCurCol(0);
+      }
+     
+      return;
+    }
+
+    if (curCol < rows[0].length) {
+      updatedRows[curRow][curCol] = key;
+      setRows(updatedRows);
+      setCurCol(curCol+1);
+    }
+    
+    
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -19,11 +75,11 @@ export default function App() {
       <Text style={styles.title}>WORDLE</Text>
       <ScrollView style={styles.map}>
 
-        {rows.map((row)=> (
-          <View style={styles.row}>
-          {row.map((cell) => (
-          <View style={styles.cell}> 
-          <Text style={styles.cellText}>{cell.toUpperCase()}</Text>
+        {rows.map((row, i)=> (
+          <View key={`row-${i}`} style={styles.row}>
+          {row.map((letter, j) => (
+          <View key={`cell-${i}-${j}`}style={[styles.cell, {borderColor: isCellActive(i, j) ? colors.grey: colors.darkgrey, backgroundColor: getCellBGColor(letter, i, j)}]}> 
+          <Text style={styles.cellText}>{letter.toUpperCase()}</Text>
           </View>
           ))}
         </View>
@@ -32,7 +88,7 @@ export default function App() {
           
         
       </ScrollView>
-      <KeyBoard />
+      <KeyBoard onKeyPressed={onKeyPressed}/>
     </SafeAreaView>
   );
 }
